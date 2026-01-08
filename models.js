6020f85653af3b6bccac9c80ee98303f0a1faab5
annycalvkkj-byte <annycalvkkj@gmail.com>
2026-01-08 18:07:22 -0300

    Update models.js

--- models.js
+++ models.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+// Esquema para Respostas Automáticas
+const ReplySchema = new mongoose.Schema({
+    keyword: String,
+    response: String
+});
+
+// Esquema para salvar mensagens recebidas
+const MessageSchema = new mongoose.Schema({
+    from: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = {
+    Reply: mongoose.model('Reply', ReplySchema),
+    Message: mongoose.model('Message', MessageSchema)
+};
